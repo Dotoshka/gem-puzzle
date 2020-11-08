2b668bfa4209e923f85c7d3ed7c7c181e64c793d
Tamara <yaroslavcevatamara@gmail.com>
2020-11-08 16:21:15 +0300

    feat: implement move method

--- gem-puzzle/src/js/script.js
+++ gem-puzzle/src/js/script.js
@@ -2,19 +2,29 @@
 
 import '../styles/styles.css';
 
-// const swap = (arr, i1, i2, j1, j2) => {
-//   const temp = arr[i1][j1];
-//   arr[i1][j1] = arr[i2][j2];
-//   arr[i2][j2] = temp;
-// };
+const createChipsArray = (size) => {
+  const arr = [];
+  for (let i = 0, k = 1; i < size; i++) {
+    arr[i] = [];
+    for (let j = 0; j < size; j++) {
+      if (i === size - 1 && j === size - 1) {
+        arr[i].push(0);
+      } else {
+        arr[i].push(k);
+        k++;
+      }
+    }
+  }
+  return arr;
+};
 
 class Game {
   constructor(size) {
     this.size = size;
     // this.field = createField();
     // this.chips = createChips();
-    this.chipsArray = this.createChipsArray(size);
-    console.log(this.chipsArray);
+    this.chipsArray = createChipsArray(size);
+    // console.log(this.chipsArray);
   }
 
   init() {
@@ -23,38 +33,49 @@ class Game {
     this.field.style.gridTemplateColumns = `repeat(${this.size}, 1fr)`;
     this.field.style.gridTemplateRows = `repeat(${this.size}, 1fr)`;
     document.body.prepend(this.field);
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
-        this.chip = document.createElement('div');
-        this.chip.classList.add('chip');
-        this.field.appendChild(this.chip);
-        this.chip.innerText = this.chipsArray[i][j];
-        this.chip.dataset.number = this.chipsArray[i][j];
-      }
-    }
-    if (this.chip.dataset.number === '0') {
-      this.chip.classList.add('empty');
-    }
     return this;
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  createChipsArray(size) {
-    const arr = [];
-    for (let i = 0, k = 1; i < size; i++) {
-      arr[i] = [];
-      for (let j = 0; j < size; j++) {
-        if (i === size - 1 && j === size - 1) {
-          arr[i].push(0);
-        } else {
-          arr[i].push(k);
-          k++;
+  createElements() {
+    let chip = '';
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        chip = document.createElement('div');
+        chip.classList.add('chip');
+        chip.innerText = this.chipsArray[i][j];
+        chip.dataset.index = `${i}-${j}`;
+        this.field.appendChild(chip);
+        if (chip.innerText === '0') {
+          chip.classList.add('empty');
+          this.emptyCoords = { x: chip.dataset.index[0], y: chip.dataset.index[2] };
         }
       }
     }
-    return arr;
+    this.chips = document.querySelectorAll('.chip');
+    // console.log(this.chips);
+    this.chips.forEach((elem) => {
+      elem.addEventListener('click', this.move);
+    });
+    // console.log(this.emptyCoords);
+  }
+
+  move = (event) => {
+    const currChip = event.target;
+    const currCoords = { x: currChip.dataset.index[0], y: currChip.dataset.index[2] };
+    const emptyChip = document.querySelector(`[data-index="${this.emptyCoords.x}-${this.emptyCoords.y}"]`);
+    console.log(currChip);
+    console.log(currCoords);
+    console.log(emptyChip);
+    if ((this.emptyCoords.x === currCoords.x && Math.abs(currCoords.y - this.emptyCoords.y) === 1)
+    || (this.emptyCoords.y === currCoords.y && Math.abs(currCoords.x - this.emptyCoords.x) === 1)) {
+      emptyChip.innerText = currChip.innerText;
+      this.emptyCoords = currCoords;
+      currChip.classList.add('empty');
+      emptyChip.classList.remove('empty');
+      console.log(this.emptyCoords);
+    }
   }
 }
 
 const size = 4;
-new Game(size).init();
+new Game(size).init().createElements();
