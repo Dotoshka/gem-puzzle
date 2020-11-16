5f0b4359466f1b81c4e4038e30634f2e6d5cfc4a
Tamara <yaroslavcevatamara@gmail.com>
2020-11-16 18:36:04 +0300

    feat: add background images

---
+++ gem-puzzle/src/js/images.js
@@ -0,0 +1,14 @@
+import bgImg1 from '../assets/images/1.jpeg';
+import bgImg2 from '../assets/images/2.jpeg';
+import bgImg3 from '../assets/images/3.jpeg';
+import bgImg4 from '../assets/images/4.jpeg';
+import bgImg5 from '../assets/images/5.jpeg';
+import bgImg6 from '../assets/images/6.jpeg';
+import bgImg7 from '../assets/images/7.jpeg';
+import bgImg8 from '../assets/images/8.jpeg';
+import bgImg9 from '../assets/images/9.jpeg';
+import bgImg10 from '../assets/images/10.jpeg';
+import bgImg11 from '../assets/images/11.jpeg';
+
+export default [bgImg1, bgImg2, bgImg3, bgImg4, bgImg5,
+  bgImg6, bgImg7, bgImg8, bgImg9, bgImg10, bgImg11];
